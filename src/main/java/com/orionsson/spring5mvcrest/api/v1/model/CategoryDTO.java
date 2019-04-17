2dd1525f8798940ef4b7075e3cfafc22ab5417c4
fatma arik copur <fatmaarik@gmail.com>
2019-04-17 12:54:49 -0400

    category mapper is developed.

--- src/main/java/com/orionsson/spring5mvcrest/api/v1/model/CategoryDTO.java
+++ src/main/java/com/orionsson/spring5mvcrest/api/v1/model/CategoryDTO.java
@@ -1,5 +1,8 @@
 package com.orionsson.spring5mvcrest.api.v1.model;
 
+import lombok.Data;
+
+@Data
 public class CategoryDTO {
     Long id;
     String name;
---
+++ src/test/java/com/orionsson/spring5mvcrest/api/v1/mapper/CategoryMapperTest.java
@@ -0,0 +1,23 @@
+package com.orionsson.spring5mvcrest.api.v1.mapper;
+
+import com.orionsson.spring5mvcrest.api.v1.model.CategoryDTO;
+import com.orionsson.spring5mvcrest.domain.Category;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CategoryMapperTest {
+    public static final String NAME = "ftm";
+    private static final Long ID= 1L;
+    CategoryMapper categoryMapper = CategoryMapper.INSTANCE;
+    @Test
+    public void categoryToCategoryTest() throws Exception{
+        Category category = new Category();
+        category.setId(1L);
+        category.setName(NAME);
+
+        CategoryDTO categoryDTO = categoryMapper.categoryToCategoryDTO(category);
+        assertEquals(ID, categoryDTO.getId());
+        assertEquals(NAME, categoryDTO.getName());
+    }
+}
