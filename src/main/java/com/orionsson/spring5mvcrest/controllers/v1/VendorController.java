8c74cfdda91f74f10e9ac132c6f1ae2763c72f8d
fatma arik copur <fatmaarik@gmail.com>
2019-04-20 15:21:04 -0400

    api documentation of vendor model is added.

--- src/main/java/com/orionsson/spring5mvcrest/controllers/v1/VendorController.java
+++ src/main/java/com/orionsson/spring5mvcrest/controllers/v1/VendorController.java
@@ -3,9 +3,12 @@ package com.orionsson.spring5mvcrest.controllers.v1;
 import com.orionsson.spring5mvcrest.api.v1.model.VendorDTO;
 import com.orionsson.spring5mvcrest.api.v1.model.VendorListDTO;
 import com.orionsson.spring5mvcrest.services.VendorService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+@Api(description = "Vendor Controller of the API")
 @RestController
 @RequestMapping(VendorController.BASE_URL)
 public class VendorController {
@@ -16,12 +19,14 @@ public class VendorController {
         this.vendorService = vendorService;
     }
 
+    @ApiOperation(value = "Lists all vendors")
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public VendorListDTO getAllVendors(){
         return vendorService.getAllVendors();
     }
 
+    @ApiOperation(value = "Return vendor according to id")
     @GetMapping({"/{id}"})
     @ResponseStatus(HttpStatus.OK)
     public VendorDTO getVendorById(@PathVariable Long id){
@@ -35,18 +40,21 @@ public class VendorController {
         return vendorService.createVendor(vendorDTO);
     }
 
+    @ApiOperation(value = "Delete vendor")
     @DeleteMapping({"/{id}"})
     @ResponseStatus(HttpStatus.OK)
     public void deleteVendor(@PathVariable Long id){
         vendorService.deleteVendorById(id);
     }
 
+    @ApiOperation(value = "Patch vendor")
     @PatchMapping({"/{id}"})
     @ResponseStatus(HttpStatus.OK)
     public VendorDTO patchVendor(@PathVariable Long id, @RequestBody VendorDTO vendorDTO){
         return vendorService.patchVendorByDTO(id,vendorDTO);
     }
 
+    @ApiOperation(value = "Put vendor")
     @PutMapping({"/{id}"})
     @ResponseStatus(HttpStatus.OK)
     public VendorDTO putVendor(@PathVariable Long id, @RequestBody VendorDTO vendorDTO){
